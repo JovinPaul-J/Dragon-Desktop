cadfe65dcb8a1c4f99405aa2679b0918cc9e30bc
JovinPaul-J <jovinpaulj@gmail.com>
2025-03-05 16:01:40 +0530

    Add files via upload

---
+++ App/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow, session } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    }
+  });
+
+  session.defaultSession.setCertificateVerifyProc((request, callback) => {
+    callback(0);
+  });
+  
+  win.loadFile('index.html');
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
